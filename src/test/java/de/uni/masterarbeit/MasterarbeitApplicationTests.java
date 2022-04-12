1fa1335c00e5610d539e43c1fad0992416ce2e24
Julia <julia_milicevic@icloud.com>
2022-04-12 13:13:31 +0200

    Spring Boot Setup

---
+++ src/test/java/de/uni/masterarbeit/MasterarbeitApplicationTests.java
@@ -0,0 +1,13 @@
+package de.uni.masterarbeit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterarbeitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
